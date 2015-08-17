7a53b6b6600af4bdfa2dd8fb806276244076e0b4
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-08-17 10:08:38 +0000

    skeleton user polls

--- client/app/polls/user-polls.directive.js
+++ client/app/polls/user-polls.directive.js
@@ -1,22 +1,18 @@
 'use strict';
 
 angular.module('basejumpApp')
-  .factory('polls', ['$http', function ($http) {
-    return {
-      getPollsForUser (id) {
-        return $http.get('/api/polls/user/' + id).then(res => res.data);
-      },
-      getPoll (id) {
-        return $http.get('/api/polls/poll/' + id).then(res => res.data);
-      }
-    };
-  }])
-  
   .controller('UserPollsCtrl', ['$scope', 'polls', 'Auth', function ($scope, polls, Auth) {
-    let userId = Auth.getCurrentUser()._id;
-    $scope.polls = null;
-    // get all polls for current user
-    polls.getPollsForUser(userId).then(polls => $scope.polls = polls);
+    $scope.polls = null; // []
+    
+    Auth.getCurrentUser().$promise.then(user => {
+      // get all polls for current user
+      return polls.getPollsForUser(user._id).then(polls => $scope.polls = polls);
+    })
+    
+    $scope.getVotes = function (id) {
+      let poll = $scope.polls.find(poll => poll._id === id);
+      return poll.options.reduce((votes, option) => votes + option.votes.length, 0);
+    };
   }])
   
   .directive('userPolls', function () {
@@ -26,8 +22,16 @@ angular.module('basejumpApp')
         <div class="user-polls" ng-controller="UserPollsCtrl">
           <div ng-show="polls && !polls.length" class="alert alert-danger">You do not currently have any polls</div>
           
-          <div ng-show="polls && polls.length" ng-repeat="poll in polls">
-            {{ poll }}
+          <div ng-show="polls && polls.length" ng-repeat="poll in polls" class="panel panel-default">
+            <div class="panel-body">
+              <span class="vote-count label label-primary">Votes: {{ getVotes(poll._id) }}</span>
+              <span>{{ poll.name }}</span>
+              <div class="buttons pull-right">
+                <button class="btn btn-primary"><i class="fa fa-eye"></i></button>
+                <button class="btn btn-success"><i class="fa fa-share-alt"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-remove"></i></button>
+              </div>
+            </div>
           </div>
         </div>
       `
